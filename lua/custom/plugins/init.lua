699a25b1f021c6007bb05d1f0833bfce7f39e12d
Thinus Løje <thil@bankingcircle.com>
2025-04-14 16:45:43 +0200

    add snacks

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+local plugins = {
+  { import = 'snacks' },
+}
+require('lazy').setup(plugins)
 return {}
